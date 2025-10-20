49ee767ff1a94870f120a6a455308190266fd176
jrgustmadtown <jrgust@wisc.edu>
2025-10-19 22:31:48 -0500

    hi

--- WIS.py
+++ WIS.py
@@ -8,37 +8,29 @@ outputy = []
 def wis(jobs):
     n = len(jobs)
     sigma = sorted(jobs, key=lambda x: x[1])    #jobs sorted by finish time
-    M = [n]
-    M[0] = 0
+    M = [0] * (n + 1)
 
-    for j in range(1, n):
-        sj, fj, vj = sigma[j]
-        rho = sorted(sigma[:j], key=lambda x: x[0]) #jobs before j sorted by start time
+    for j in range(1, n + 1): 
+        sj, fj, vj = sigma[j - 1]  
+       
+        p = 0
+        for i in range(j - 1):  # check previous jobs
+            si, fi, vi = sigma[i]
+            if fi <= sj:
+                p = i + 1   
 
-        #find largest finish time that is before sj
-        max_fi = -float("inf")
-        i=0
-        for si, fi, vi in rho:
-            if max_fi <= fi and fi <= sj:
-                max_fi = fi
-            i+=1
-            
-        #populate
-        if max_fi == -float("inf"):
-            M[i] = 0
-        else:
-            M[j] = max(M[j-1], M[i] + vj)
+        M[j] = max(vj + M[p], M[j - 1]) #bellman
 
-    return M[n-1]
+    return M[n]
 
 for _ in range(k):
     jobs = []
-
-    for __ in range(index):
-        index+=1
+    n = int(inputy[index])   
+    index += 1
+    for __ in range(n):
         s, f, v = inputy[index].split()
         jobs.append([int(s), int(f), int(v)])
-        index+=1
+        index += 1
 
     outputy.append(wis(jobs))
 
